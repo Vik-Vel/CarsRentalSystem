b8f26c1d1219649e1d6e83fb7519abdddacbd470
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-06-17 21:58:14 +0300

    Project files added

---
+++ CarsRentalSystem/CarsRentalSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarsRentalSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
